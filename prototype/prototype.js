03f0f550250e5cd16f3eb99e5338b420672861a5
chap95 <chap95@likelion.org>
2021-09-11 10:44:51 +0900

    add: prototype content

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -10,3 +10,5 @@ const person1 = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiing']);
 
 console.log('person1.__proto__ : ', person1.__proto__);
 console.log('Person.prototype : ', Person.prototype);
+console.log('Object.prototype : ', Object.prototype);
+// 브라우저에서는 무엇이 뜨던데 여기에서는 왜 비어있을까?
